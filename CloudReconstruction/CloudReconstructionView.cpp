910962743da62036f1e697bbbfeab33b6c167312
Liang Fang <fangliang1313@gmail.com>
2021-01-12 15:41:36 +0800

    编译vtk[qt], 替换vcpkg包

--- CloudReconstruction/CloudReconstructionView.cpp
+++ CloudReconstruction/CloudReconstructionView.cpp
@@ -23,6 +23,7 @@
 IMPLEMENT_DYNCREATE(CCloudReconstructionView, CView)
 
 BEGIN_MESSAGE_MAP(CCloudReconstructionView, CView)
+	ON_WM_DESTROY()
 END_MESSAGE_MAP()
 
 // CCloudReconstructionView 构造/析构
@@ -32,16 +33,18 @@ CCloudReconstructionView::CCloudReconstructionView() noexcept
 	// TODO: 在此处添加构造代码
 	m_cloud.reset((new pcl::PointCloud<pcl::PointXYZ>));
 	m_viewer.reset(new pcl::visualization::PCLVisualizer("viewer", false));//初始化viewer对象
-
+	
+	m_iren = vtkSmartPointer<vtkRenderWindowInteractor>::New(); //初始化vtkwindow交互的对象 
 
 }
 
 CCloudReconstructionView::~CCloudReconstructionView()
 {
-	//delete m_cloud.get();
-	//delete m_viewer.get();
-	//m_win->Delete();
+	// 此处的内存释放无用，网上说为 interactor 无法释放， release下正常
+	//m_iren->SetRenderWindow(nullptr);
+	//m_iren->SetInteractorStyle(nullptr);
 	//m_iren->Delete();
+	
 }
 
 BOOL CCloudReconstructionView::PreCreateWindow(CREATESTRUCT& cs)
@@ -95,30 +98,30 @@ void CCloudReconstructionView::OnInitialUpdate()
 
 	// TODO: 在此添加专用代码和/或调用基类
 	//m_viewer.reset(new pcl::visualization::PCLVisualizer("viewer", false));//初始化viewer对象
-	m_viewer->addCoordinateSystem();           //设置对应的坐标系
+	//m_viewer->addCoordinateSystem();           //设置对应的坐标系
 	m_viewer->setBackgroundColor(0, 0, 0);     //设置背景颜色
 	m_viewer->initCameraParameters();          //初始化相机的参数
-	m_win = m_viewer->getRenderWindow();       //将view中的渲染窗口的句柄传递给vtk window
-	m_iren = vtkRenderWindowInteractor::New(); //初始化vtkwindow交互的对象 
+	m_rwnd = m_viewer->getRenderWindow();       //将view中的渲染窗口的句柄传递给vtk window
+	//m_iren = vtkRenderWindowInteractor::New(); //初始化vtkwindow交互的对象 
 	m_viewer->resetCamera();                   //使点云显示在屏幕中间，并绕中心操作
 
 	LoadPCDfile();                             //载入点云数据：m_viewer->addPointCloud
 
 	CRect rect;
 	this->GetClientRect(&rect);                //实时获取MFC窗口大小
-	m_win->SetSize(rect.right - rect.left, rect.bottom - rect.top); //根据当前窗口的大小设置vtk 窗口的大小
-	m_win->SetParentId(this->m_hWnd);          //设置vtk窗口的句柄
-	m_win->SetPosition(0, 0);
-	m_iren->SetRenderWindow(m_win);            //将vtk交互对象与vtk window绑定 
+	m_rwnd->SetSize(rect.right - rect.left, rect.bottom - rect.top); //根据当前窗口的大小设置vtk 窗口的大小
+	m_rwnd->SetParentId(this->m_hWnd);          //设置vtk窗口的句柄
+	m_rwnd->SetPosition(0, 0);
+	m_iren->SetRenderWindow(m_rwnd);            //将vtk交互对象与vtk window绑定 
 	m_viewer->createInteractor();
-	m_win->Render();                           //开始渲染
-
+	//m_viewer->setupInteractor(m_iren, m_win);
+	m_rwnd->Render();                           //开始渲染
+	
 }
 
 
 void CCloudReconstructionView::LoadPCDfile()
 {
-	//pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 	std::string filename = "rabbit.pcd";
 	if (pcl::io::loadPCDFile(filename, *m_cloud) == -1) {
 		PCL_ERROR("Couldn't read file");
@@ -126,7 +129,16 @@ void CCloudReconstructionView::LoadPCDfile()
 	}
 	m_viewer->setBackgroundColor(0.0, 0.0, 0.0);
 	m_viewer->removeAllPointClouds();//该函数不加则不显示
-	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color(m_cloud, 0, 255, 0);
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color(m_cloud, 0, 255, 0);  //设置点云颜色
 	m_viewer->addPointCloud<pcl::PointXYZ >(m_cloud, single_color, "sample cloud");
+	m_viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+}
+
+
+void CCloudReconstructionView::OnDestroy()
+{
+	CView::OnDestroy();
+
+	// TODO: 在此处添加消息处理程序代码
 
 }
--- CloudReconstruction/CloudReconstructionView.h
+++ CloudReconstruction/CloudReconstructionView.h
@@ -4,16 +4,19 @@
 
 #pragma once
 
+#include <memory>
+
 //  PCL
 #undef min 
 #undef max 
 #include <pcl/console/parse.h>
-#include <pcl/io/io.h>
-#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/common/io.h>
 #include <pcl/visualization/pcl_visualizer.h>
 //  VTK
+#include <vtkSmartPointer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 
@@ -33,11 +36,10 @@ public:
 	void LoadPCDfile();
 
 private:
-	pcl::PointCloud<pcl::PointXYZ>::Ptr m_cloud;
-	boost::shared_ptr<pcl::visualization::PCLVisualizer> m_viewer;//要共享指针类型的，要不然，显示窗口会跳出MFC界面
-	vtkRenderWindow* m_win;   //vtk渲染的窗口句柄
-	vtkRenderWindowInteractor* m_iren;//vtk交互的对象
-
+	pcl::PointCloud<pcl::PointXYZ>::Ptr m_cloud;                       //pcl点云数据
+	std::shared_ptr<pcl::visualization::PCLVisualizer> m_viewer;       //pcl可视化对象，应使用共享智能指针否则窗口会独立
+	vtkSmartPointer<vtkRenderWindow> m_rwnd;                            //vtk渲染的窗口句柄
+	vtkSmartPointer<vtkRenderWindowInteractor> m_iren;                 //vtk交互的对象:鼠标、键盘
 
 // 重写
 public:
@@ -61,6 +63,7 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	virtual void OnInitialUpdate();
+	afx_msg void OnDestroy();
 };
 
 #ifndef _DEBUG  // CloudReconstructionView.cpp 中的调试版本
